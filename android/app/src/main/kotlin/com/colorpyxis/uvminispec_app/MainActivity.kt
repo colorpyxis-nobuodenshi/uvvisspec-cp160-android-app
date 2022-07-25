b19977916580112557fed058a2b0ed0bfb9c0b0e
mfujino <mfujino@colorpyxis.com>
2022-07-25 12:43:50 +0900

    change 2022/7/25.

--- android/app/src/main/kotlin/com/colorpyxis/uvminispec_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.colorpyxis.uvvisspec_insects_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/colorpyxis/uvvisspec_cp160/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.colorpyxis.uvvisspec_cp160
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
